fa5f572b71450bfc12cb13d9df267c86c243ed96
yrqt <35965448+yrqt@users.noreply.github.com>
2018-06-16 08:43:37 +0200

    Create class.panier.php

---
+++ class/class.panier.php
@@ -0,0 +1,21 @@
+<?php 
+class panier{
+	public function __construct(){
+		if(!isset($_SESSION)){
+			session_start();
+		}
+		if(!isset($_SESSION['panier'])){
+			$_SESSION['panier'] = array();
+		}
+	}
+
+	public function add($product_id){
+		$_SESSION['panier'][$product_id] = 1;
+
+	}
+
+
+
+}
+
+ ?>
