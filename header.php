e1fe60c9217dbf25bbc7604b188d9f4f9037c82b
yrqt <35965448+yrqt@users.noreply.github.com>
2018-06-16 08:45:29 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,11 @@
+<?php 
+require '_header.php';
+?><!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Mam Hapi and Co</title>
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+	<link rel='icon' type='image/ico' href='./images/favicon.ico'>
+</head>
+<body>
